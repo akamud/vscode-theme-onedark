d6558a7f3ecc17ad1e402bf13685fd16515ab1e3
Mahmoud Ali <muddibr@gmail.com>
2017-01-15 13:21:16 -0200

    More JS/TS Samples

--- samples/sample.js
+++ samples/sample.js
@@ -1,4 +1,3 @@
-
 // shim layer with setTimeout fallback
 window.requestAnimFrame = (function(){
 	return  window.requestAnimationFrame       ||
@@ -19,3 +18,57 @@ window.requestAnimFrame = (function(){
 })();
 // place the rAF *before* the render() to assure as close to
 // 60fps with the setTimeout fallback.
+
+(function(angular) {
+  'use strict';
+
+  angular.module('chrome')
+    .factory('historyService', historyService);
+
+  function historyService($q, sharedService) {
+    return {
+      historyList: historyList,
+      getVisits: getVisits,
+      monitorHistory: monitorHistory,
+    };
+
+    function historyList(param) {
+      var chromeParam = {};
+      chromeParam.text = param.searchText;
+      chromeParam.startTime = param.startDate;
+      chromeParam.maxResults = param.maxResults;
+      var deferred = $q.defer();
+      chrome.history.search(chromeParam,
+        function(response) {
+          var list = sharedService.populateList(response);
+          deferred.resolve(list);
+        });
+      return deferred.promise;
+    }
+
+    function getVisits(url) {
+      var deferred = $q.defer() && true;
+      chrome.history.getVisits({ url: url }, function(visits) {
+        deferred.resolve(visits);
+      });
+      return deferred.promise;
+    }
+
+    function monitorHistory(addHistoryCB) {
+      chrome.history.onVisited.addListener(function(historyItem) {
+        addHistoryCB(historyItem);
+      });
+    }
+  }
+
+})(angular);
+
+
+const options = {
+	method: 'GET',
+	uri: 'https://api.github.com/com/search/repositores',
+	headers: {
+		'User-Agent': 'gittr-cli',
+	},
+	qs: { q: `language:${argv.lang}`, sort: 'stars'},
+};
--- samples/sample.ts
+++ samples/sample.ts
@@ -21,3 +21,58 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(disposable);
 }
+
+
+(function(angular) {
+  'use strict';
+
+  angular.module('chrome')
+    .factory('historyService', historyService);
+
+  function historyService($q, sharedService) {
+    return {
+      historyList: historyList,
+      getVisits: getVisits,
+      monitorHistory: monitorHistory,
+    };
+
+    function historyList(param) {
+      var chromeParam = {};
+      chromeParam.text = param.searchText;
+      chromeParam.startTime = param.startDate;
+      chromeParam.maxResults = param.maxResults;
+      var deferred = $q.defer();
+      chrome.history.search(chromeParam,
+        function(response) {
+          var list = sharedService.populateList(response);
+          deferred.resolve(list);
+        });
+      return deferred.promise;
+    }
+
+    function getVisits(url) {
+      var deferred = $q.defer();
+      chrome.history.getVisits({ url: url }, function(visits) {
+        deferred.resolve(visits);
+      });
+      return deferred.promise;
+    }
+
+    function monitorHistory(addHistoryCB) {
+      chrome.history.onVisited.addListener(function(historyItem) {
+        addHistoryCB(historyItem);
+      });
+    }
+  }
+
+})(angular);
+
+
+const options = {
+	method: 'GET',
+	uri: 'https://api.github.com/com/search/repositores',
+	headers: {
+		'User-Agent': 'gittr-cli',
+	},
+	qs: { q: `language:${argv.lang}`, sort: 'stars'},
+};
